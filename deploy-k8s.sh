0c08400db6d5461a6f50e1179e4159886c478bfc
EC2 Default User <ec2-user@ip-172-31-7-151.ap-south-1.compute.internal>
2025-12-13 10:22:42 +0000

    add eks deploy script

--- deploy-k8s.sh
+++ deploy-k8s.sh
@@ -5,21 +5,13 @@ REGION="ap-south-1"
 CLUSTER="brain-tasks-eks"
 NAMESPACE="default"
 
-DOCKERHUB_USERNAME="aravindkumar18"
-IMAGE_NAME="brain-tasks-app"
-
-IMAGE_TAG=$(cat imageTag.txt)
-FULL_IMAGE="$DOCKERHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
-
-echo "Deploying image: $FULL_IMAGE"
-
 aws eks update-kubeconfig --region $REGION --name $CLUSTER
 
+IMAGE_TAG=$(cat imageTag.txt)
+IMAGE="aravindkumar18/brain-tasks-app:$IMAGE_TAG"
+
 kubectl set image deployment/brain-tasks-deployment \
-  brain-tasks-container=$FULL_IMAGE \
-  -n $NAMESPACE
+brain-tasks-container=$IMAGE \
+-n $NAMESPACE
 
-kubectl rollout status deployment/brain-tasks-deployment \
-  -n $NAMESPACE --timeout=300s
-
-echo "✅ Deployment successful"
+kubectl rollout status deployment/brain-tasks-deployment -n $NAMESPACE
