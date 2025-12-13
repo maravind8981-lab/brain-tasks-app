39d590998e41e4905e67fe04497ae02e34ca6f68
EC2 Default User <ec2-user@ip-172-31-7-151.ap-south-1.compute.internal>
2025-12-13 09:55:39 +0000

    update

--- deploy-k8s.sh
+++ deploy-k8s.sh
@@ -1,42 +1,25 @@
 #!/bin/bash
 set -e
 
-# -------- CONFIG --------
 REGION="ap-south-1"
-CLUSTER_NAME="brain-tasks-eks"
+CLUSTER="brain-tasks-eks"
 NAMESPACE="default"
 
-# Docker Hub image (CHANGE USERNAME)
-DOCKERHUB_USERNAME="maravind8981lab"
+DOCKERHUB_USERNAME="aravindkumar18"
 IMAGE_NAME="brain-tasks-app"
 
-# Image tag passed from CodeBuild
-IMAGE_TAG=${IMAGE_TAG:-latest}
-
+IMAGE_TAG=$(cat imageTag.txt)
 FULL_IMAGE="$DOCKERHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
 
-echo "-----------------------------------"
 echo "Deploying image: $FULL_IMAGE"
-echo "Cluster: $CLUSTER_NAME"
-echo "Namespace: $NAMESPACE"
-echo "-----------------------------------"
 
-# -------- Update kubeconfig --------
-echo "Updating kubeconfig..."
-aws eks update-kubeconfig \
-  --region $REGION \
-  --name $CLUSTER_NAME
+aws eks update-kubeconfig --region $REGION --name $CLUSTER
 
-# -------- Deploy to Kubernetes --------
-echo "Updating deployment image..."
 kubectl set image deployment/brain-tasks-deployment \
   brain-tasks-container=$FULL_IMAGE \
   -n $NAMESPACE
 
-# -------- Verify rollout --------
-echo "Waiting for rollout to complete..."
 kubectl rollout status deployment/brain-tasks-deployment \
-  -n $NAMESPACE \
-  --timeout=300s
+  -n $NAMESPACE --timeout=300s
 
 echo "✅ Deployment successful"
